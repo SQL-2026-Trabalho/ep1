353cc442b15cf8d73751aa9a20c1f0765f075832
espalinha <matheusespalaor14@gmail.com>
2026-05-06 21:50:35 -0300

    Criação das tabelas que seram indexadas com diferentes tipos

---
+++ create_indexes/create_b_plus_tables/create_b_plus_tables_psql.sql
@@ -0,0 +1,27 @@
+CREATE TABLE usuarios_b_plus AS
+SELECT *
+FROM Users;
+CREATE TABLE posts_b_plus AS
+SELECT *
+FROM Posts;
+CREATE TABLE badges_b_plus AS
+SELECT *
+FROM Badges;
+CREATE TABLE comments_b_plus AS
+SELECT *
+FROM Comments;
+CREATE TABLE posttypes_b_plus AS
+SELECT *
+FROM PostTypes;
+CREATE TABLE linktypes_b_plus AS
+SELECT *
+FROM LinkTypes;
+CREATE TABLE postlinks_b_plus AS
+SELECT *
+FROM PostLinks;
+CREATE TABLE votetypes_b_plus AS
+SELECT *
+FROM VoteTypes;
+CREATE TABLE votes_b_plus AS
+SELECT *
+FROM Votes;
